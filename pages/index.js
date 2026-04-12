14d60b18375d166df0b4098e316767264fc0d028
Libor Hoření <liborhoreni@gmail.com>
2026-04-13 00:06:30 +0200

    Load Inter font via link tag instead of CSS import

--- pages/index.js
+++ pages/index.js
@@ -117,6 +117,9 @@ export default function Home() {
         <meta name="description" content="Denní menu restaurací v okolí JIC Brno" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </Head>
 
       <header className={styles.header}>
